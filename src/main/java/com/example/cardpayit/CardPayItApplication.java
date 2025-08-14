a0b199abd7f474db54230b492786f8d3f1159ec7
VVBoss <growder1@gmail.com>
2025-08-15 01:37:39 +0500

    Init

---
+++ src/main/java/com/example/cardpayit/CardPayItApplication.java
@@ -0,0 +1,13 @@
+package com.example.cardpayit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardPayItApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardPayItApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cardpayit/CardPayItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cardpayit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardPayItApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
